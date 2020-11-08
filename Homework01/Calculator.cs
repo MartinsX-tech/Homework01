fe0973d3ff1f95061bcba26aaa08cda44e2f7586
MartinsX-tech <martins.atauga@gmail.com>
2020-11-08 16:19:25 +0200

    Add project files.

---
+++ Homework01/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Homework01
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ievadi skaitli x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ievadi skaitli y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ievadi aritmētisko operāciju: ");
+            string z = Console.ReadLine();
+
+            if (z == "+")
+            {
+                Console.WriteLine(Utils.Add(x, y));
+            } else if (z == "-")
+            {
+                Console.WriteLine(Utils.Subtract(x, y));
+            } else if (z == "*")
+            {
+                Console.WriteLine(Utils.Multiply(x, y));
+            } else if (z == "/")
+            {
+                Console.WriteLine(Utils.Divide(x, y));
+            }
+
+        }
+    }
+}
---
+++ Homework01/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework01
+{
+    class Utils
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
